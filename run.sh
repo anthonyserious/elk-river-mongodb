276f5acb95e5ce513c62f087291062f0fccfcaf8
Anthony Schneider <anthonyserious@gmail.com>
2014-12-09 21:38:50 -0500

    run.sh change.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec docker run --name elktest -v `pwd`/logs:/logs -v `pwd`data:/data -p 5601:5601 -p 27018:27017 -p 9201:9200 -d anthonyserious/elktest
+exec docker run --name elkmongo -v `pwd`/logs:/logs -v `pwd`data:/data -p 5601:5601 -p 27018:27017 -p 9201:9200 -d anthonyserious/elk-river-mongodb
 
