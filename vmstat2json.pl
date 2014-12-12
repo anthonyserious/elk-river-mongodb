da2d83c2cb0d52decf1609a429c5617e5533cb9f
Anthony Schneider <anthonyserious@gmail.com>
2014-12-11 21:04:04 -0500

    Fix up vmstat log and vmstat2json - strings values now numeric, and ignore second header.

--- vmstat2json.pl
+++ vmstat2json.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl
 
 while ($line = <>) {
-  next if $line =~ /^procs/;
+  next if $line =~ /^(procs| r)/;
   
   my @keys = ("r", "b", "swpd", "free", "buff", "cache", "si", "so", "bi", "bo", "in", "cs", "us", "sy", "id", "wa");
   my @a  = split (' ', $line);
   my @vals = ();
   foreach $k (@keys) {
-    push (@vals, "\"$k\":\"".shift(@a)."\"");
+    push (@vals, "\"$k\":".shift(@a));
   }
   print "{".join(", ", @vals)."}\n";
 }
